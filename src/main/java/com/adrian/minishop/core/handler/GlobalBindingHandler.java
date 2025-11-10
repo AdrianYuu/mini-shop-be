cc5c130ad5bbee470484564dd14d795593a0a967
Adrian Yu <adrian.yu001@gmail.com>
2025-11-10 15:05:44 +0700

    chore: change controller advice class name

--- src/main/java/com/adrian/minishop/core/handler/GlobalBindingHandler.java
+++ src/main/java/com/adrian/minishop/core/handler/GlobalBindingHandler.java
@@ -4,12 +4,13 @@ import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
-public class TrimHandler {
+public class GlobalBindingHandler {
 
     @InitBinder
-    public void initBinder(WebDataBinder binder) {
+    public void initBinder(WebDataBinder binder, WebRequest request) {
         binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 
