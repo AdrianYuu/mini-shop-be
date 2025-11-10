6d13ef0b4b4c72438e47784bc3717e2a992461ba
Adrian Yu <adrian.yu001@gmail.com>
2025-11-10 15:06:16 +0700

    feat: added cors in security config

--- src/main/java/com/adrian/minishop/config/SecurityConfig.java
+++ src/main/java/com/adrian/minishop/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                         .csrfTokenRepository(cookieCsrfTokenRepository)
                         .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
                 )
+                .cors(cors -> {
+                })
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
